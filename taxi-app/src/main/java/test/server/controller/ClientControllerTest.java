19b006a8750d73f9a57f0ae39d7a9f465ee4455d
Polyakov Igor <polykovio@mail.ru>
2018-05-27 21:31:52 +0300

    Added tests for ClientController

--- taxi-app/src/main/java/test/server/controller/ClientControllerTest.java
+++ taxi-app/src/main/java/test/server/controller/ClientControllerTest.java
@@ -1,36 +1,124 @@
 package test.server.controller;
 
-import com.ibm.icu.impl.Assert;
+import com.google.gwt.thirdparty.json.JSONArray;
+import com.google.gwt.thirdparty.json.JSONException;
+import com.google.gwt.thirdparty.json.JSONObject;
 import com.taxi.server.model.Client;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 import com.taxi.shared.dto.ClientDto;
 
 public class ClientControllerTest {
 
-    public List<Client> getAllClients() throws ClientProtocolException, IOException
+    public int parseContent(String str, int startPos, List<ClientDto> clients) {
+
+        String subStr = str.substring(startPos);
+        int pos = 0;
+        ++pos; // skip '['
+
+        ClientDto client = new ClientDto();
+
+        {
+            String value = "";
+            while (subStr.charAt(pos) != ',') {
+                value = value + subStr.charAt(pos);
+                ++pos;
+            }
+            ++pos; // skip ','
+            client.setId(Integer.parseInt(value));
+        }
+
+        {
+            String value = "";
+            while (subStr.charAt(pos) != ',') {
+                value = value + subStr.charAt(pos);
+                ++pos;
+            }
+            ++pos; // skip ','
+            String subValue = value.substring(1, value.length() - 1);
+            client.setName(subValue);
+        }
+
+        {
+            String value = "";
+            while (subStr.charAt(pos) != ',') {
+                value = value + subStr.charAt(pos);
+                ++pos;
+            }
+            ++pos; // skip ','
+            String subValue = value.substring(1, value.length() - 1);
+            client.setPassword(subValue);
+        }
+
+        {
+            String value = "";
+            while (subStr.charAt(pos) != ']') {
+                value = value + subStr.charAt(pos);
+                ++pos;
+            }
+            ++pos; // skip ']'
+            String subValue = value.substring(1, value.length() - 1);
+            client.setDescription(subValue);
+        }
+
+        clients.add(client);
+
+        return pos;
+    }
+
+    public List<ClientDto> getAllClients() throws ClientProtocolException, IOException
     {
         HttpUriRequest request = new HttpGet(
                 "http://127.0.0.1:8888/api/client/get/all" );
         HttpResponse response =
                 HttpClientBuilder.create().build().execute(request);
-        List<Client> clients = RestTestUtil.retrieveResourceFromResponse(
-                response, List.class);
+        HttpEntity entity = response.getEntity();
+        String entityString = EntityUtils.toString(entity);
+        entityString = entityString.substring(1, entityString.length() - 1);
+        List<ClientDto> clients = new ArrayList<ClientDto>();
+        int i = 0;
+        while (i < entityString.length())
+        {
+            i += parseContent(entityString, i, clients);
+            ++i; // skip ','
+        }
 
         return clients;
+    }
+
+    public void createNewClient() throws ClientProtocolException, IOException
+    {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("http://127.0.0.1:8888/api/client/create");
+
+        String params = "{\"name\":\"cl1\",\"password\":\"cl1\",\"description\":\"abc\"}";
+        StringEntity entity = new StringEntity(params);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setEntity(entity);
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        org.junit.Assert.assertEquals(response.getStatusLine().getStatusCode(),
+                HttpStatus.SC_OK);
+        httpClient.close();
     }
 
     public void deleteClient(int ID) throws ClientProtocolException, IOException
@@ -52,40 +140,26 @@ public class ClientControllerTest {
         HttpUriRequest request = new HttpGet("http://127.0.0.1:8888/api/client/get/1");
 
         HttpResponse response = HttpClientBuilder.create().build().execute(request);
-        Client client = RestTestUtil.retrieveResourceFromResponse(
-                response, Client.class);
+        String clientStr = EntityUtils.toString(response.getEntity());
 
-        org.junit.Assert.assertNotEquals(null, client);
+        org.junit.Assert.assertNotEquals(null, clientStr);
     }
 
     @Test
     public void createClient() throws ClientProtocolException, IOException {
-        // Creating of client
-        ClientDto clientDto = new ClientDto();
-        clientDto.setName("cl1");
-        clientDto.setPassword("cl1");
-        clientDto.setDescription("abc");
-        HttpPost request = new HttpPost(
-                "http://127.0.0.1:8888/api/client/create");
-        request.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        HttpEntity entity = new StringEntity(clientDto.toString());
-        request.setEntity(entity);
-        HttpResponse response =
-                HttpClientBuilder.create().build().execute(request);
-        org.junit.Assert.assertEquals(response.getStatusLine().getStatusCode(),
-                HttpStatus.SC_OK);
+        createNewClient();
 
-        // Check of creating client
-        List<Client> clients = getAllClients();
+        // Check of creating new client
+        List<ClientDto> clients = getAllClients();
         boolean isOk = false;
         int ID;
-        for (Client client : clients) {
-            if((client.getName() == "cl1") &&
-               (client.getPassword() == "cl1") &&
-               (client.getDescription() == "abc"))
+        for (ClientDto client : clients) {
+            if((client.getName().equals("cl1")) &&
+               (client.getPassword().equals("cl1")) &&
+               (client.getDescription().equals("abc")))
             {
                 isOk = true;
-                ID = client.getID();
+                ID = client.getId();
 
                 // Deleting of new client
                 deleteClient(ID);
@@ -101,14 +175,134 @@ public class ClientControllerTest {
     }
 
     @Test
-    public void updateClient() {
+    public void updateClient() throws ClientProtocolException, IOException {
+        createNewClient();
+
+        // Find new client
+        int ID = 0;
+        {
+            List<ClientDto> clients = getAllClients();
+            for (ClientDto client : clients) {
+                if ((client.getName().equals("cl1")) &&
+                        (client.getPassword().equals("cl1")) &&
+                        (client.getDescription().equals("abc"))) {
+                    ID = client.getId();
+                    break;
+                }
+            }
+        }
+
+        // Update client
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("http://127.0.0.1:8888/api/client/update");
+
+        String params = "{\"id\":\"" + Integer.toString(ID) + "\"," + "\"name\":\"cl1\",\"password\":\"cl1\",\"description\":\"cba\"}";
+        StringEntity entity = new StringEntity(params);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setEntity(entity);
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        org.junit.Assert.assertEquals(response.getStatusLine().getStatusCode(),
+                HttpStatus.SC_NO_CONTENT);
+        httpClient.close();
+
+        // Check of updating new client
+        List<ClientDto> clients = getAllClients();
+        boolean isOk = false;
+        for (ClientDto client : clients) {
+            if((client.getName().equals("cl1")) &&
+               (client.getDescription().equals("cba")))
+            {
+                isOk = true;
+
+                // Deleting of new client
+                deleteClient(ID);
+
+                break;
+            }
+        }
+
+        if(!isOk)
+        {
+            org.junit.Assert.assertTrue(false);
+        }
     }
 
     @Test
-    public void login() {
+    public void login() throws ClientProtocolException, IOException {
+        createNewClient();
+
+        // Get id of new client
+        int ID = 0;
+        {
+            List<ClientDto> clients = getAllClients();
+            for (ClientDto client : clients) {
+                if ((client.getName().equals("cl1")) &&
+                        (client.getPassword().equals("cl1")) &&
+                        (client.getDescription().equals("abc"))) {
+                    ID = client.getId();
+                    break;
+                }
+            }
+        }
+
+        // Login
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("http://127.0.0.1:8888/api/client/login");
+
+        String params = "{\"username\":\"cl1\",\"password\":\"cl1\"}";
+        StringEntity entity = new StringEntity(params);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+        request.setEntity(entity);
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        org.junit.Assert.assertEquals(response.getStatusLine().getStatusCode(),
+                HttpStatus.SC_NO_CONTENT);
+        httpClient.close();
+
+        // Delete new client
+        deleteClient(ID);
     }
 
     @Test
-    public void delete() {
+    public void delete() throws ClientProtocolException, IOException {
+        createNewClient();
+
+        // Get id of new client
+        int ID = 0;
+        {
+            List<ClientDto> clients = getAllClients();
+            for (ClientDto client : clients) {
+                if ((client.getName().equals("cl1")) &&
+                    (client.getPassword().equals("cl1")) &&
+                    (client.getDescription().equals("abc"))) {
+                    ID = client.getId();
+                    break;
+                }
+            }
+        }
+
+        // Deleting of new client
+        deleteClient(ID);
+
+        // Check of deleting new client
+        {
+            List<ClientDto> clients = getAllClients();
+            boolean isOk = true;
+            for (ClientDto client : clients) {
+                if ((client.getName().equals("cl1")) &&
+                    (client.getPassword().equals("cl1")) &&
+                    (client.getDescription().equals("abc"))) {
+                    isOk = false;
+                    break;
+                }
+            }
+
+            if (!isOk) {
+                org.junit.Assert.assertTrue(false);
+            }
+        }
     }
 }
